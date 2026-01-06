15c47a4c38b71c16d5166c2d165bb155e4086e02
Vegard Schøyen Houg <vegard.houg@revolve.no>
2026-01-06 18:07:09 +0100

    3: Fix shared variable handling in C and Go example

--- Exercise1/3 - shared variable/c/foo.c
+++ Exercise1/3 - shared variable/c/foo.c
@@ -3,26 +3,42 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int i = 0;
 
 // Note the return type: void*
+// We want to protext the shared variable i, so mutexes are the best choice.
+// We only want one thread to operate on the variable i at a time.
+
+pthread_mutex_t lock;
+
+// Lock the mutex before performing operations on i, and unlock after.
 void* incrementingThreadFunction(){
     for (int j = 0; j < 1000000; j++){
+        pthread_mutex_lock(&lock);
         i++;
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     for (int j = 0; j < 1000000; j++){
+        pthread_mutex_lock(&lock);
         i--;
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
 
 
 int main(){
+    if (pthread_mutex_init(&lock, NULL) != 0){
+        printf("\nMutex initialization failed\n");
+        return EXIT_FAILURE;
+    }
+
     pthread_t* thread1;
     pthread_create(&thread1, NULL, incrementingThreadFunction, NULL);
 
@@ -33,5 +49,8 @@ int main(){
     pthread_join(thread2, NULL);
     
     printf("The magic number is: %d\n", i);
+
+    pthread_mutex_destroy(&lock);
+
     return 0;
 }
