69ae0c0e5bf0567ca0c11ef304e7f6a3df78fded
haavagr <hovard.groenvold@gmail.com>
2026-02-22 13:00:36 +0100

    typo lights

--- elevator/lights/lights.go
+++ elevator/lights/lights.go
@@ -2,10 +2,11 @@ package lights
 
 import (
 	"elevator/config"
+	"elevator/distributor"
 	"elevator/elevio"
 )
 
-func SetLights(commonState elevator.CommonState, ElevatorID int) {
+func SetLights(commonState distributor.CommonState, ElevatorID int) {
 	for floor := 0; floor < config.NumFloors; floor++ {
 		for buttonType := 0; buttonType < 2; buttonType++ {
 			if commonState.HallRequests[floor][buttonType] {
@@ -16,7 +17,7 @@ func SetLights(commonState elevator.CommonState, ElevatorID int) {
 		}
 	}
 	for floor := 0; floor < config.NumFloors; floor++ {
-		if commonState.States[ElevatorID].CabRequests[floor] {
+		if commonState.LocalStates[ElevatorID].CabRequests[floor] {
 			elevio.SetButtonLamp(elevio.BT_Cab, floor, true)
 		} else {
 			elevio.SetButtonLamp(elevio.BT_Cab, floor, false)
